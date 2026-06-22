ff7e44a8223fcf1283834489db1fd9b8db12ddca
Tibirlayn <tibirlayn@gmail.com>
2026-06-23 02:35:32 +0300

    netology_catalog_phones

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255) # название модели телефона
+    price = models.IntegerField() # цена телефона
+    image = models.URLField() # ссылка на изображение телефона
+    release_date = models.DateField() # дата выхода модели телефона
+    lte_exists = models.BooleanField() # наличие LTE
+    slug = models.SlugField(unique=True) # уникальный идентификатор модели телефона
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
